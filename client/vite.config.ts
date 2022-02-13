d832dde80bf4af04698e708f682778c17eb8f3c8
Erick ZV <zv.erick@protonmail.com>
2022-02-13 17:59:52 -0600

    Add path alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
+// @ts-ignore path alias gives error but is ok
+import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	resolve: {
+		alias: {
+			$components: path.resolve(__dirname, './src/components'),
+			$assets: path.resolve(__dirname, './src/assets'),
+		},
+	},
 });
