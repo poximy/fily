bbb71294267206db5ad73f890824e124680db9e0
ponti <pontiggia>
2022-02-13 01:45:34 -0300

    created models

---
+++ server/src/models/models.py
@@ -0,0 +1,38 @@
+import strawberry
+from typing import List, Literal
+
+
+# Product model
+@strawberry.type
+class Product:
+    user_id: int
+    product_id: int
+    category: str
+    name: str
+    description: str
+    total_buy: float  # price for buy the product inmediately
+    actual_bid: float
+    remaining_time: float
+    bid_status: Literal["active", "inactive", "finished"]
+    bid_quantity: int
+    creation_date: str
+
+
+# User model
+@strawberry.type
+class User:
+    user_id: str  # unique Nickname 
+    pseudonimo: str
+    password: str
+    user_list: List[Product]
+    money: float
+    seller: bool
+    freeze_money: float
+    seller_list: List[Product]
+    reputation: float
+
+
+    
+
+
+
