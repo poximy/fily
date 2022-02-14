ed4fa16b95f0b79b2dd61bee5aa523db7b886ba6
Erick ZV <zv.erick@protonmail.com>
2022-02-13 20:45:38 -0600

    Convert to Next.js

---
+++ client/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Home: NextPage = () => {
+	return <p>Index</p>;
+};
+
+export default Home;
--- client/tailwind.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	content: ['./index.html', './src/**/*.{svelte, html}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
