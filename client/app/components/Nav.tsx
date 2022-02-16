a2cf56a1b5536166a6b7fd91721f4e3a0a1f4f9d
Brayan-724 <bjqf0724@gmail.com>
2022-02-15 20:29:01 -0700

    Change Navbar component

--- client/app/components/Nav.tsx
+++ client/app/components/Nav.tsx
@@ -1,22 +1,73 @@
-import { Button, Flex } from '@chakra-ui/react'
-import { FC } from 'react';
+import { Button, Flex, IconButton, useColorModeValue, useMediaQuery } from '@chakra-ui/react';
+import { FC, useMemo } from 'react';
+import Icon from './Icon';
 
-const Nav: FC = () => {
-	// TODO change this value
-	const coins = 1000
+export type ActiveTab =
+	| 'search'
+	| 'basket'
+	| 'trending'
+	| 'notifications'
+	| 'profile';
+export interface INavProps {
+	activeTab: ActiveTab;
+}
+
+const Nav: FC<INavProps> = ({ activeTab }) => {
+	const styleBg = useColorModeValue('#FEFEFE', 'neutral.100');
 
 	return (
-			<Flex as='nav' bg='blue.200' justify='center' mb={3}>
-				<Flex w='full' maxW='1024px' justify='space-between'>
-					<Button rounded='none' cursor='pointer' colorScheme='blue'>
-						Cuenta
-					</Button>
-					<Button rounded='none' cursor='pointer' colorScheme='blue'>
-						$ {coins}
-					</Button>
-				</Flex>
+		<Flex
+			as='nav'
+			bg={styleBg}
+			justify='center'
+			mb={2}
+			boxShadow='0 -1px #AAA'
+			px={4}
+			py={4}
+		>
+			<Flex width='100%' justify='space-around'>
+				<NavButton active={activeTab === 'search'}>search</NavButton>
+				<NavButton active={activeTab === 'basket'}>shopping_basket</NavButton>
+				<NavButton active={activeTab === 'trending'}>trending_up</NavButton>
+				<NavButton active={activeTab === 'notifications'}>
+					notifications
+				</NavButton>
+				<NavButton active={activeTab === 'profile'}>person</NavButton>
 			</Flex>
+		</Flex>
 	);
 };
+
+interface INavButtonProps {
+	active: boolean;
+	children: string;
+}
+
+function NavButton({ active, children }: INavButtonProps) {
+	const [isSmall] = useMediaQuery('(max-width: 256px)');
+	const sizes = useMemo(
+		() => ({
+			active: '44px',
+			normal: '36px',
+		}),
+		[],
+	);
+
+	return (
+		<IconButton
+			color={active ? 'principal.orange' : 'neutral.400'}
+			variant='ghost'
+			mr={isSmall ? 0 : 3}
+			aria-label='A'
+			icon={
+				<Icon
+					transition='font-size .3s ease'
+					fontSize={active ? sizes.active : sizes.normal}
+					name={children}
+				/>
+			}
+		/>
+	);
+}
 
 export default Nav;
