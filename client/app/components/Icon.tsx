07fb0dbd38859be1f6e8c4f3c3b7e5239abcd586
Brayan-724 <bjqf0724@gmail.com>
2022-02-15 20:30:03 -0700

    Change/Add components to sync with Figma design

---
+++ client/app/components/Icon.tsx
@@ -0,0 +1,27 @@
+import { Text, TextProps } from '@chakra-ui/react';
+import * as React from 'react';
+
+interface IIconProps extends TextProps {
+	name: string;
+	variant?: 'filled' | 'outlined' | 'rounded' | 'sharp';
+}
+
+const Icon: React.FunctionComponent<IIconProps> = ({ name, ...props }) => {
+	return (
+		<Text
+			as='span'
+      fontWeight='400'
+      fontSize='24px'
+			{...props}
+			css={{
+				fontFamily: `"Material Icons${
+					(props.variant ?? 'filled') === 'filled' ? '' : ' ' + props.variant
+				}"`,
+			}}
+		>
+			{name}
+		</Text>
+	);
+};
+
+export default Icon;
