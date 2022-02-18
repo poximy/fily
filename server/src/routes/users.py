0a99fdda301afad419e8f518a32e206da3f357f0
ponti <pontiggia>
2022-02-18 19:37:29 -0300

    Add users route

---
+++ server/src/routes/users.py
@@ -0,0 +1,57 @@
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException, Request
+from ..auth import hash_password
+
+
+router = APIRouter()
+
+
+class PostUser(BaseModel):
+    user_id: int
+    pseudonimo: str
+    email: str
+    phone: Optional[str] = None
+    password: str
+    seller: bool = False
+    reputation: float = 0
+    money: float = 0
+    freeze_money: float = 0
+
+
+@router.post("/signup")
+async def sign_up(user: PostUser, request: Request):
+    # check if user already exists
+    sqlite = request.state.sqlite
+    # create table if not exists
+    cursor = await sqlite.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY AUTOINCREMENT, pseudonimo TEXT, email TEXT, phone TEXT, password TEXT, seller BOOLEAN, reputation FLOAT, money FLOAT, freeze_money FLOAT)")
+    cursor = await sqlite.execute("SELECT * FROM users WHERE user_id = ?", (user.user_id,))
+    await sqlite.commit()
+    row = await cursor.fetchone()
+    if row:
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    # create user
+    cursor = await sqlite.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                                  (user.user_id, user.pseudonimo, user.email, user.phone, hash_password(user.password),
+                                   user.seller, user.reputation, user.money, user.freeze_money))
+    await sqlite.commit()
+
+    return {"user created": user.pseudonimo}
+
+
+@router.post("/login")
+async def login(user: PostUser, request: Request):
+    # check if user exists
+    sqlite = request.state.sqlite
+    cursor = await sqlite.execute("SELECT * FROM users WHERE user_id = ?", (user.user_id,))
+    await sqlite.commit()
+    row = await cursor.fetchone()
+    if not row:
+        raise HTTPException(status_code=400, detail="User does not exist")
+
+    # check if password is correct
+    if not row[4] == hash_password(user.password):
+        raise HTTPException(status_code=400, detail="Incorrect password")
+
+    return {"user": row[1]}
