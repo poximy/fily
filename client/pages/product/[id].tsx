128c258c889e484dc559feae8966fe1da6f55823
Brayan-724 <bjqf0724@gmail.com>
2022-02-17 22:07:47 -0700

    [Client, No ended] Update view product screen

--- client/pages/product/[id].tsx
+++ client/pages/product/[id].tsx
@@ -1,33 +1,54 @@
 import MainLayout from '@app/layouts/Main';
 import { IProduct } from '@app/typings/product';
+import { sanityRoute } from '@app/utils/route';
 import { Box, Flex, Text } from '@chakra-ui/react';
+import Icon from '@components/Icon';
 import { GetServerSideProps, NextPage } from 'next';
-import { useRouter } from 'next/router';
 import * as React from 'react';
 
-const ProductScreen: NextPage = props => {
-	const router = useRouter();
-	const id = router.query.id as string;
+interface ProductScreenProps {
+	product: IProduct | null;
+}
 
-	const product = {
-		id: 1,
-		name: 'Product 1',
-		category: 'videogames',
-    currentBid: 100
-	} as IProduct;
-  const starts = 4.2;
+const ProductScreen: NextPage<ProductScreenProps> = ({ product }) => {
+	if (!product) {
+		return (
+			<MainLayout>
+				<Flex justify='center' align='center' pt={10}>
+					<Text fontSize='3xl' fontWeight='500' color='GrayText'>
+						Product not found
+					</Text>
+				</Flex>
+			</MainLayout>
+		);
+	}
+
+	const stars = 4.7;
 
 	return (
 		<MainLayout>
-			<Flex justify='space-between'>
-				<Text as='span'>Subastador</Text>
-				<Flex>
-					<Text as='span'>{starts}</Text>
-          <Flex>
-            {[...Array(5)].map((_, i) => (
-              <Box key={i}>A</Box>
-            ))}
-          </Flex>
+			<Flex justify='space-between' align='center'>
+				<Text as='span' fontWeight='500' fontSize='lg'>
+					Subastador
+				</Text>
+				<Flex align='center' gap={2}>
+					<Text as='span' fontWeight='700' fontSize='md'>
+						{stars}
+					</Text>
+					<Flex>
+						{[...Array(5)].map((_, i) => (
+							<Icon
+								key={i}
+								name={
+									i + 1 <= stars
+										? 'star'
+										: i + 1 <= stars + 0.5
+										? 'star_half'
+										: 'star_border'
+								}
+							/>
+						))}
+					</Flex>
 				</Flex>
 			</Flex>
 			<Box>a</Box>
@@ -40,24 +61,31 @@ const ProductScreen: NextPage = props => {
 
 export default ProductScreen;
 
+export const getServerSideProps: GetServerSideProps = async context => {
+	const id = (context.query.id as string).split('-')[0];
+	const productName = (context.query.id as string)
+		.split('-')
+		.slice(1)
+		.join('-');
+	const res = await fetch(
+		`http://localhost:3000/api/placeholder/products/${id}`,
+	);
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const id = context.query.id as string;
-  const product = await fetch(`http://localhost:3000/api/placeholder/products/${id}`);
+	const product = (await res.json()) as IProduct;
 
-  if(product.status === 404) {
-    context.res.statusCode = 404;
+	if (!res.ok || sanityRoute(product.name) !== productName) {
+		context.res.statusCode = 404;
 
-    return {
-      props: {
-        product: null
-      }
-    }
-  }
+		return {
+			props: {
+				product: null,
+			},
+		};
+	}
 
-  return {
-    props: {
-      product
-    }
-  }
+	return {
+		props: {
+			product,
+		},
+	};
 };
