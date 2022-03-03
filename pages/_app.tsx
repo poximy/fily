110055693a711e3be19ab322c467b5b55e83c1ec
poximy <76846708+poximy@users.noreply.github.com>
2022-03-03 17:57:49 -0600

    Add auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
+import { SessionProvider } from 'next-auth/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import theme from '../styles/theme';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
 		<ChakraProvider resetCSS theme={theme}>
-			<Component {...pageProps} />
+			<SessionProvider session={session}>
+				<Component {...pageProps} />
+			</SessionProvider>
 		</ChakraProvider>
 	);
 }
