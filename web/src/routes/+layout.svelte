88e6fad42a7124ce24486d89f060edab41cdf439
poximy <76846708+poximy@users.noreply.github.com>
2022-12-20 16:10:55 -0600

    [refactor] remove unnecessary classes

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -16,23 +16,21 @@
 <svelte:window on:keydown={handleKeydown} />
 
 <nav
-  class="flex w-full flex-col gap-y-2 gap-x-4 font-sans sm:flex-row sm:items-center sm:px-4"
+  class="flex w-full flex-col gap-y-2 gap-x-4 px-4 font-sans sm:flex-row sm:items-center"
 >
   <div
-    class="relative z-10 mx-4 h-10 flex-auto before:absolute before:top-1
-    before:left-1 before:h-full before:w-full before:bg-sky-400 sm:m-0"
+    class="relative h-10 flex-auto before:absolute before:top-1 before:left-1
+	  before:h-full before:w-full before:bg-black"
   >
     <input
       type="text"
       spellcheck="false"
       placeholder="Buscar marca, color, etc."
-      class="absolute z-10 h-full w-full rounded-none border border-black p-2 focus:outline-none"
+      class="absolute h-full w-full rounded-none border-2 border-black p-2 focus:outline-none"
       bind:this={searchBar}
     />
   </div>
-  <ul
-    class="flex h-10 items-center justify-center gap-4 bg-neutral-200 sm:h-auto sm:bg-inherit"
-  >
+  <ul class="flex justify-center gap-4 text-lg">
     <li>
       <a href="/" class="hover:underline">Tendencias</a>
     </li>
